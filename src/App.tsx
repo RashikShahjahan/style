19783958d9f4b2b59f8eeda68546459aae477b82
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-09-24 18:16:15 -0400

    grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Post from "./Post"
 import TaskList from "./TaskList"
+import Grid from "./Grid"
 
 function App() {
 
   return (
     <div>
-      <Post/>
+      <Grid/>
     </div>
   )
 }
